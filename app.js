069ae38238e7dc899cec1c50c10ed979965db296
Mooortie <Mattias.stromon@gmail.com>
2023-02-17 10:31:25 +0100

    da

--- app.js
+++ app.js
@@ -10,31 +10,30 @@ const PORT = process.env.PORT ||3000
 
 
 const animeList = { anime: [
-    { id:1, Name: "One Piece", Episodes: "1050",Completed: "Caught Up", Raiting: "8/10"},
-    { id:2, Name: "Bleach", Episodes: "379", Completed: "Caught Up", Raiting: "7/10"},
-    { id:3, Name: "Dragonball ", Episodes: "153", Completed: "Caught Up", Raiting: "8/10"},
-    { id:4, Name: "Dragonball Z", Episodes: "291", Completed: "Caught Up", Raiting: "6/10"},
-    { id:5, Name: "Dragonball Super", Episodes: "131", Completed: "Dropped", Raiting: "5/10"},
-    { id:6, Name: "Attack on Titan", Episodes: "87", Completed: "Caught Up", Raiting: "6/10"},
-    { id:7, Name: "Code Geass", Episodes: "50", Completed: "Caught Up", Raiting: "10/10"},
-    { id:8, Name: "BlueLock", Episodes: "24", Completed: "Caught Up", Raiting: "8/10"},
-    { id:9, Name: "Spy x Family", Episodes: "25", Completed: "Caught Up", Raiting: "6/10"},
-    { id:10, Name: "Hunter x Hunter", Episodes: "148", Completed: "Caught Up", Raiting: "4/10"},
-    { id:11, Name: "Steins;Gate", Episodes: "24", Completed: "Caught Up", Raiting: "8/10"},
-    { id:12, Name: "Your Name", Episodes: "Movie", Completed: "Caught Up", Raiting: "9/10"},
-    { id:13, Name: "Vinland Saga", Episodes: "24", Completed: "Caught Up", Raiting: "6/10"},
-    { id:14, Name: "Jujustu Kaisen", Episodes: "24", Completed: "Dropped", Raiting: "5/10"},
-    { id:15, Name: "Haikyuu", Episodes: "85", Completed: "Caught Up", Raiting: "8/10"},
-    { id:16, Name: "Tengen Toppa Gurren Lagann", Episodes: "27", Completed: "Caught Up", Raiting: "9/10"},
-    { id:17, Name: "Cyberpunk: Edgerunners", Episodes: "10", Completed: "Caught Up", Raiting: "8/10"},
-    { id:18, Name: "Death Note", "Episodes": "37", Completed: "Caught Up", Raiting: "9/10"},
-    { id:19, Name: "Neon Genesis Evangelion", Episodes: "26", Completed: "Caught Up", Raiting: "7/10"},
-    { id:20, Name: "JoJo's Bizarre Adventure", Episodes: "190", Completed: "Caught Up", Raiting: "9/10"},
-    { id:21, Name: "One Punch Man", Episodes: "24", Completed: "Caught Up", Raiting: "6/10"},
-    { id:22, Name: "Demon Slayer", Episodes: "44", Completed: "Caught Up", Raiting: "8/10"},
-    { id:23, Name: "K-On", Episodes: "39", Completed: "Caught Up", Raiting: "6/10"}
-]}
-
+    { id:1, name: "One Piece", episodes: "1050",completed: "Caught Up", raiting: "8/10"},
+    { id:2, name: "Bleach", episodes: "379", completed: "Caught Up", raiting: "7/10"},
+    { id:3, name: "Dragonball ", episodes: "153", completed: "Caught Up", raiting: "8/10"},
+    { id:4, name: "Dragonball Z", episodes: "291", completed: "Caught Up", raiting: "6/10"},
+    { id:5, name: "Dragonball Super", episodes: "131", Ccmpleted: "Dropped", raiting: "5/10"},
+    { id:6, name: "Attack on Titan", episodes: "87", completed: "Caught Up", raiting: "6/10"},
+    { id:7, name: "Code Geass", episodes: "50", completed: "Caught Up", raiting: "10/10"},
+    { id:8, name: "BlueLock", episodes: "24", completed: "Caught Up", raiting: "8/10"},
+    { id:9, name: "Spy x Family", episodes: "25", completed: "Caught Up", raiting: "6/10"},
+    { id:10, name: "Hunter x Hunter", episodes: "148", completed: "Caught Up", raiting: "4/10"},
+    { id:11, name: "Steins;Gate", episodes: "24", completed: "Caught Up", raiting: "8/10"},
+    { id:12, name: "Your Name", episodes: "Movie", completed: "Caught Up", raiting: "9/10"},
+    { id:13, name: "Vinland Saga", episodes: "24", completed: "Caught Up", raiting: "6/10"},
+    { id:14, name: "Jujustu Kaisen", episodes: "24", completed: "Dropped", raiting: "5/10"},
+    { id:15, name: "Haikyuu", episodes: "85", completed: "Caught Up", Raiting: "8/10"},
+    { id:16, name: "Tengen Toppa Gurren Lagann", episodes: "27", completed: "Caught Up", raiting: "9/10"},
+    { id:17, name: "Cyberpunk: Edgerunners", episodes: "10", completed: "Caught Up", raiting: "8/10"},
+    { id:18, name: "Death Note", episodes: "37", completed: "Caught Up", raiting: "9/10"},
+    { id:19, name: "Neon Genesis Evangelion",episodes: "26", completed: "Caught Up", raiting: "7/10"},
+    { id:20, name: "JoJo's Bizarre Adventure", episodes: "190", completed: "Caught Up", raiting: "9/10"},
+    { id:21, name: "One Punch Man", episodes: "24", completed: "Caught Up", raiting: "6/10"},
+    { id:22, name: "Demon Slayer", episodes: "44", completed: "Caught Up", raiting: "8/10"},
+    { id:23, name: "K-On", episodes: "39", completed: "Caught Up", raiting: "6/10"}
+]
 app.get("/",(req,res) => {
     res.sendFile(__dirname + "/index.html")
 })
@@ -57,12 +56,12 @@ app.get("/addanime",(req,res) => {
 
 app.post("/addanime", (req,res)=>{
     
-    let newName = req.body.Name
+    let newName = req.body.name
     let newEpisodes = req.body.Episodes
     let newCompleted = req.body.Completed
     let newRaiting = req.body.Raiting
 
-    animeList.anime.push({id: animeList.anime.length + 1, Name : newName, Episodes : newEpisodes, Completed: newCompleted, Raiting: newRaiting})
+    animeList.anime.push({id: animeList.anime.length + 1, name : newName, Episodes : newEpisodes, Completed: newCompleted, Raiting: newRaiting})
 })
 
 app.delete("/animelist/:id", (req, res) => {
